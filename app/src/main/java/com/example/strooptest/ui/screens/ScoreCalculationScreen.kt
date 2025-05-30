ab39d3e2f842d5a8c4cfa990643559b32ad75b04
Vineet Kumar <vineetprofessional1530@gmail.com>
2025-05-31 00:00:33 +0530

    Clean Code

--- app/src/main/java/com/example/strooptest/ui/screens/ScoreCalculationScreen.kt
+++ app/src/main/java/com/example/strooptest/ui/screens/ScoreCalculationScreen.kt
@@ -3,7 +3,6 @@ package com.example.strooptest.ui.screens
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
